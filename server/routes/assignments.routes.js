09cb97398e0d33fa52e9dbf652af57b5554f8033
tanishq4141 <jaintanishq4141@gmail.com>
2025-10-25 23:12:55 +0530

    check error in auth

--- server/routes/assignments.routes.js
+++ server/routes/assignments.routes.js
@@ -11,18 +11,18 @@ import { authenticate, requireTeacher } from "../middleware/auth.middleware.js";
 const router = express.Router();
 
 // Teacher creates assignment (can optionally include batchId to auto-assign)
-router.post("/create", authenticate, requireTeacher, createAssignment);
+router.post("/create",  requireTeacher, createAssignment);
 
 // Teacher assigns an existing assignment to student(s) or batch
-router.post("/assign", authenticate, requireTeacher, assignAssignment);
+router.post("/assign",  requireTeacher, assignAssignment);
 
 // Student submits answers
-router.post("/submit", authenticate, submitAssignment);
+router.post("/submit",  submitAssignment);
 
 // Student or authorized user gets student assignments
-router.get("/student/:id", authenticate, getStudentAssignments);
+router.get("/student/:id",  getStudentAssignments);
 
 // Get assignments created by teacher
-router.get("/teacher/:id", authenticate, getTeacherAssignments);
+router.get("/teacher/:id",  getTeacherAssignments);
 
 export default router;
--- server/routes/batch.routes.js
+++ server/routes/batch.routes.js
@@ -4,7 +4,7 @@ import { authenticate, requireTeacher } from "../middleware/auth.middleware.js";
 
 const batchRouters = express.Router();
 
-batchRouters.post("/create", authenticate, requireTeacher, createBatch);
-batchRouters.get("/", authenticate, requireTeacher, getBatches);
+batchRouters.post("/create", requireTeacher, createBatch);
+batchRouters.get("/", requireTeacher, getBatches);
 
 export default batchRouters;
